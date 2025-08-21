ad68caf6696ffd776b07987e0ab52845c91c6092
biel.barros <gabrielbarrosg11@gmail.com>
2025-08-21 15:18:28 -0300

    feat: Introduce enums

---
+++ PaisesAPI/PaisesAPI/Models/Enums.cs
@@ -0,0 +1,18 @@
+namespace PaisesAPI.Models
+{
+    public enum Sexo 
+    { 
+        Masculino = 1, 
+        Feminino = 2, 
+        Outro = 3 
+    }
+    
+    public enum EstadoCivil 
+    { 
+        Solteiro = 1, 
+        Casado = 2, 
+        Divorciado = 3, 
+        Viuvo = 4, 
+        UniaoEstavel = 5 
+    }
+}
